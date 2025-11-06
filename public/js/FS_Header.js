d4064e3cf8bc12d57912570cf7e74d38252f3e2b
MerlijnVS-2470127 <merlijn.vansuetendael@student.uhasselt.com>
2025-11-06 13:56:44 +0100

    Account creation added

--- public/js/FS_Header.js
+++ public/js/FS_Header.js
@@ -1,32 +1,42 @@
 function logout(){
-    localStorage.removeItem("username");
-    let username = localStorage.getItem("username");
-    let usernameDiv = document.getElementById("username");
-    usernameDiv.innerText = username;
+    sessionStorage.removeItem("user");
+    localStorage.removeItem("user");
+    checkLogin();
 }
 
-function login(){
-    localStorage.setItem("username", "gog");
-    let username = localStorage.getItem("username");
-    let usernameDiv = document.getElementById("username");
-    usernameDiv.innerText = username;
+function checkUsername(){
+    if (localStorage.getItem("user") != null) {
+        let username = localStorage.getItem("user");
+        let usernameDiv = document.getElementById("username");
+        usernameDiv.innerText = username;
+    }
+    if (sessionStorage.getItem("user") != null) {
+        let username = sessionStorage.getItem("user");
+        let usernameDiv = document.getElementById("username");
+        usernameDiv.innerText = username;
+    }
+    
 }
 
-    let username = localStorage.getItem("username");
-    let usernameDiv = document.getElementById("username");
-    usernameDiv.innerText = username;
-
+function checkLogin(){
+    if (localStorage.getItem("user") == null) {
+        if (sessionStorage.getItem("user") == null) {
+            window.location.href = "/login";
+        }
+    }
+}
 
 
 let logoutButton = document.getElementById("btn_logout");
 let loginButton = document.getElementById("btn_login");
 
 logoutButton.addEventListener("click", e => {
-        logout();
-    })
+    logout();
+}) 
 
-loginButton.addEventListener("click", e => {
-        login();
-    })
+addEventListener("load", e => {
+    checkLogin();
+    checkUsername();
+})
 
 
