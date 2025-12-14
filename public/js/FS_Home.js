0476bbee0bb3b6c8191f8d3e36fed54c3c953c50
MerlijnVS-2470127 <merlijn.vansuetendael@student.uhasselt.com>
2025-12-14 21:45:12 +0100

    home page working

--- public/js/FS_Home.js
+++ public/js/FS_Home.js
@@ -5,7 +5,16 @@
 let username = window.username;
 
 addEventListener("load", (e) => {
-    let cookieString = "username=" + username + ";";
-    cookieString += " path=/;";
-    document.cookie = cookieString;
+  alert(username);
+  let cookieString = "username=" + username + ";";
+  cookieString += " path=/;";
+  document.cookie = cookieString;
+
+  let buttons = document.getElementsByClassName("group-button");
+
+  for (const button of buttons) {
+    button.addEventListener("click", (e) => {
+      window.location.href = "/groups/" + encodeURIComponent(button.id);
+    });
+  }
 });
